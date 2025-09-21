115a5aeab1bd0498887978458c60c9300e4c5188
Shaoyu Wang <shaoyuwang119@gmail.com>
2025-09-21 03:13:42 -0500

    reformatted website

--- script.js
+++ script.js
@@ -43,13 +43,13 @@ function generateCalendar(id) {
   calendar.innerHTML = html;
 }
 
-function getCurrentDay(id) {
+function getCurrentTime() {
   const monthNames = [
     "january", "february", "march", "april", "may", "june",
     "july", "august", "september", "october", "november", "december"
   ];
 
-  const dayNames = ["monday", "tuesday", "wednesday", "thursday", "friday", "saturday", "sunday"];
+  const dayNames = ["sunday", "monday", "tuesday", "wednesday", "thursday", "friday", "saturday"];
 
   const monthtxt = document.getElementById("month");
   const datetxt = document.getElementById("date");
@@ -57,12 +57,69 @@ function getCurrentDay(id) {
 
   const now = new Date();
   const month = monthNames[now.getMonth()];
-  const weekday = dayNames[now.getDay()-4];
+  const weekday = dayNames[now.getDay()];
   
   monthtxt.innerHTML = month;
   datetxt.innerHTML = now.getDate();
   daytxt.innerHTML = weekday;
+
+  const timetxt = document.getElementById("time");
+  const hrs = now.getHours().toString().padStart(2, "0");
+  const mins = now.getMinutes().toString().padStart(2,"0");
+  const secs = now.getSeconds().toString().padStart(2,"0");
+  timetxt.innerHTML = `${hrs}:${mins}:${secs}`;
 }
 
-arrangeHeader()
-getCurrentDay("daily");
+function icon(iconCode) {
+  const map = {
+    "01d": "☀️",
+    "01n": "🌙",
+    "02d": "🌤️",
+    "02n": "🌤️",
+    "03d": "☁️",
+    "03n": "☁️",
+    "04d": "☁️",
+    "04n": "☁️",
+    "09d": "🌧️",
+    "09n": "🌧️",
+    "10d": "🌦️",
+    "10n": "🌦️",
+    "11d": "⛈️",
+    "11n": "⛈️",
+    "13d": "❄️",
+    "13n": "❄️",
+    "50d": "🌫️",
+    "50n": "🌫️"
+  };
+  return map[iconCode] || "❓";
+}
+
+async function getWeather() {
+  const weatherBox = document.getElementById("weather")
+  const apiKey = "159001cf1170d6fe61928fb5b63060da"
+  const url = `https://api.openweathermap.org/data/2.5/weather?q=Austin,US&units=imperial&appid=${apiKey}`;
+  
+  try {
+    const res = await fetch(url);
+    const data = await res.json();
+
+    const temp = Math.round(data.main.temp);
+    const desc = data.weather[0].description;
+    const icons = icon(data.weather[0].icon);
+    
+    weatherBox.innerHTML = `
+      <div style="text-align:center; gap:10px; margin-top:-10px">
+        <b>${icons}austin, tx</b> - ${temp}°f
+        </div>
+      </div>
+    `;
+  } catch (e) {
+    console.error(e);
+    weatherBox.innerHTML = "Unable to load weather content.";
+  }
+}
+
+
+arrangeHeader();
+setInterval(getCurrentTime, 200);
+getWeather();
